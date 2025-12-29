36e8dca5d83ecf65fae909899b1068651757f339
wtr <w.inou.24.8.1574@4nonome.com>
2025-12-29 21:04:17 +0900

    add TemplateSelfAction

---
+++ Assets/Scripts/Common/ScriptableObject/SelfStatusAction/Template/TemplateSelfAction.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TemplateSelfAction", menuName = "SelfStatusAction/TemplateSelfAction")]
+public class TemplateSelfAction : SelfStatusAction
+{
+    public override void Execute(GameObject user)
+    {
+        //なんか入れる。
+        throw new System.NotImplementedException();
+    }
+}
