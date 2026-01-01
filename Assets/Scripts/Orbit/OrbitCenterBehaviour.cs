3ef8b606e1eb3b5c1e522104119bcb316196ea96
wtr <w.inou.24.8.1574@4nonome.com>
2026-01-01 23:12:32 +0900

    add AttackOrbit
    設定されたオブジェクトの周りを回転する衛星を作成した。

---
+++ Assets/Scripts/Orbit/OrbitCenterBehaviour.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class OrbitCenterBehaviour : MonoBehaviour
+{
+    [Header("Orbit Settings")]
+    [SerializeField] float radius = 2.5f;
+    [SerializeField] int count = 3;
+
+    [Header("Rotation")]
+    [SerializeField] float rotateSpeed = 180f;
+
+    [Header("Spawn")]
+    [SerializeField] GameObject orbitChildPrefab;
+    [SerializeField] GameObject centerObject;
+
+    void Start()
+    {
+        if (centerObject != null)
+        {
+            transform.position = centerObject.transform.position;
+            transform.SetParent(centerObject.transform);
+        }
+        Spawn();
+    }
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+    }
+
+    void Spawn()
+    {
+        float angleStep = 360f / count;
+
+        for (int i = 0; i < count; i++)
+        {
+            float angle = angleStep * i;
+            float rad = angle * Mathf.Deg2Rad;
+
+            Vector3 localPos = new Vector3(
+                Mathf.Cos(rad) * radius,
+                Mathf.Sin(rad) * radius,
+                0f
+            );
+
+            GameObject child = Instantiate(
+                orbitChildPrefab,
+                transform
+            );
+
+            child.transform.localPosition = localPos;
+        }
+    }
+}
