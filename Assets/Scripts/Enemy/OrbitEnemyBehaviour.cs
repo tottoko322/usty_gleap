9e5095ef22b6018a480a696b52195c2d6857ab5b
TanakaRukia <tanaka.rukia.558@s.kyushu-u.ac.jp>
2026-02-06 20:44:49 +0900

    Scripts

---
+++ Assets/Scripts/Enemy/OrbitEnemyBehaviour.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+
+public class OrbitEnemyBehaviour : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float stopDistance = 1f;
+
+    [Header("Orbit (optional)")]
+    [SerializeField] private OrbitCenterBehaviour orbitCenter;
+    [SerializeField] private GameObject orbitCenterPrefab;
+
+    private Transform player;
+    private float speed;
+    private bool isGameOver = false;
+    private StatusManager statusManager;
+    private StatusActionHolder statusActionHolder;
+    private GenerateGrave generateGrave;
+    private SelfStatusAction deathAction;
+
+    void Awake()
+    {
+        if (orbitCenter == null)
+        {
+            orbitCenter = GetComponentInChildren<OrbitCenterBehaviour>();
+        }
+
+        if (orbitCenter == null && orbitCenterPrefab != null)
+        {
+            GameObject instance = Instantiate(orbitCenterPrefab, transform.position, Quaternion.identity, transform);
+            orbitCenter = instance.GetComponent<OrbitCenterBehaviour>();
+        }
+
+        if (orbitCenter != null)
+        {
+            orbitCenter.SetupCenter(transform);
+        }
+    }
+
+    void Start()
+    {
+        statusManager = GetComponent<StatusManager>();
+        generateGrave = GetComponent<GenerateGrave>();
+        statusActionHolder = GetComponent<StatusActionHolder>();
+        if (statusActionHolder != null)
+        {
+            deathAction = statusActionHolder.GetSelfStatusActionFromIndex(0);
+        }
+
+        GameObject playerObject = GameObject.FindWithTag("Player");
+        player = playerObject != null ? playerObject.transform : null;
+    }
+
+    void Update()
+    {
+        if (statusManager != null)
+        {
+            speed = statusManager.GetSpeed();
+        }
+
+        if (player == null || isGameOver)
+        {
+            GameObject playerObject = GameObject.FindWithTag("Player");
+            if (playerObject == null) return;
+            player = playerObject.transform;
+            return;
+        }
+
+        Vector3 direction = (player.position - transform.position).normalized;
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (distance > stopDistance)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+
+        if (statusManager != null && statusManager.BaseStatus.CurrentHP <= 0)
+        {
+            if (generateGrave != null)
+            {
+                generateGrave.Generate(transform.position);
+            }
+        }
+
+        if (deathAction != null)
+        {
+            deathAction.Execute(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && false)
+        {
+            Debug.Log("Game Over!");
+            isGameOver = true;
+
+            if (collision.gameObject.TryGetComponent<PlayerController>(out var playerScript))
+            {
+                playerScript.enabled = false;
+            }
+
+            this.enabled = false;
+        }
+    }
+}
