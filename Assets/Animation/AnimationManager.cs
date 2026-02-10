9d4a41bf663f8f600f3fb7fd02d6de48ec39e520
tottoko322 <mercury8ly@icloud.com>
2026-02-11 04:54:21 +0900

    lose

--- Assets/Animation/AnimationManager.cs
+++ Assets/Animation/AnimationManager.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class AnimationManager : MonoBehaviour
 {
-    
-    void Start()
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    private bool isJumping = false;
+
+    void Awake()
     {
-        Animator Controller = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    
     void Update()
     {
-        
+        // 動いているかどうか
+        bool isMoving = rb.linearVelocity.sqrMagnitude > 0.1f;
+
+        // ★ 動き始めた瞬間だけ Trigger を撃つ
+        if (isMoving && !isJumping)
+        {
+            anim.SetTrigger("Jump"); // ← Trigger名（スペースNG）
+            isJumping = true;
+        }
+
+        // ★ 止まったらリセット
+        if (!isMoving)
+        {
+            isJumping = false;
+        }
     }
 }
